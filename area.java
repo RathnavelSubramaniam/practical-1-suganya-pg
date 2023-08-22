8959c28126646c17881987c5128023072af4efc2
Suganya S <54302095+suganya-pg@users.noreply.github.com>
2023-08-22 06:59:42 +0000

    completed

--- area.java
+++ area.java
@@ -3,8 +3,7 @@ public static void main(String args[]) {
 		double PI =3.14159 ;
 		double radius=10;
 	    double area = PI * radius * radius;
-	     System.out.println("The area of the circle with radius " + radius + " is: " + area);
-	        
+	     System.out.println("The area of the circle with radius " + radius + " is : " + area);  
 	     
 	    }
 	}
